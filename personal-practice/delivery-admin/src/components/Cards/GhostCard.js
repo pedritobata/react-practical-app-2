ae63137eab678b15f65d0dcd5543a0363f43674d
unknown <pedromartinez_goyo@hotmail.com>
2020-07-07 18:50:37 -0500

    Login form created and styling - customizing bootstrap fails in Windows

--- personal-practice/delivery-admin/src/components/Cards/GhostCard.js
+++ personal-practice/delivery-admin/src/components/Cards/GhostCard.js
@@ -3,8 +3,13 @@ import './GhostCard.css';
 
 const ghostCard = props => {
 
+    const customStyles = {
+        width: `${props.width}%`,
+        padding: `${props.padding}px`
+    }
+
     return (
-        <div className="ghost-card">
+        <div className="ghost-card" style={customStyles}>
             {props.children}
         </div>
     );
--- personal-practice/delivery-admin/src/pages/Auth/Auth.js
+++ personal-practice/delivery-admin/src/pages/Auth/Auth.js
@@ -9,14 +9,22 @@ const auth = props => {
         <Fragment>
             <section className="auth-page">
                 <div className="leftScreen halfScreen">
-                   <h1 className="h1 display-3 text-light">Delivery Admin</h1>
-                   <p className="h2 text-muted">El mejor sistema para administrar tu delivery</p>
+                    <h1 className="h1 display-3 text-light">Delivery Admin</h1>
+                    <p className="h4 text-white text-center">El mejor sistema para administrar tu delivery</p>
                 </div>
                 <div className="rightScreen halfScreen">
-                    <GhostCard>
+                    <GhostCard width="65" padding="35">
                         <h1 className="great-title">Login Form</h1>
-                        <button className="btn btn-send-button btn-lg">Cancel</button>
-                        <button className="btn btn-light btn-lg">Save</button>
+                        <form>
+                            <div className="form-group"><label /* htmlFor="txt-email" */ className="form-text">Email</label>
+                            <input id="txt-email" type="text" className="form-control"/></div>
+                            <div className="form-group"><label /* htmlFor="txt-email" */ className="form-text">Password</label>
+                            <input id="txt-pass" type="password" className="form-control"/></div>
+                           {/*  <button type="submit" className="btn btn-danger btn-md">Cancel</button> */}
+                            <div className="mt-4">
+                                <button type="submit" className="btn btn-primary btn-md btn-block">Send</button>
+                            </div>
+                        </form>
                     </GhostCard>
                 </div>
             </section>
