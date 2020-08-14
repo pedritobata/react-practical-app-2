73181e5932aa2acc73f6f270cbd54d0bea8f1e86
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-08-14 01:02:49 -0500

    Sidebar component - first part

--- spotify-like-app/src/components/Sidebar/Sidebar.js
+++ spotify-like-app/src/components/Sidebar/Sidebar.js
@@ -5,7 +5,9 @@ const Sidebar = () => {
 
     return (
         <div className="sidebar">
-            <h1>Sidebar</h1>
+            <img 
+            src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+            className="sidebar__logo" />
         </div>
     );
 }
