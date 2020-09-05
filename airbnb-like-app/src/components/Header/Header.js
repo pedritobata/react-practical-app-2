67430478d9472a665c3943cfce2d89af9e223f3f
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-09-05 15:54:22 -0500

    airbnb project creation

---
+++ airbnb-like-app/src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+
+    return (
+        <header className="header">
+            <img src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" 
+                className="header__logo"
+                alt="logo"/>
+        </header>
+    );
+}
+
+export default Header;
