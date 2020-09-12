7f5fde3776a95e2fd83714721a8fb31e7824a0d0
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-09-12 00:51:50 -0500

    Search page first commit

--- airbnb-like-app/src/App.js
+++ airbnb-like-app/src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
-import Footer from './components/Footer/Footer';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import SearchPage from "./pages/SearchPage/SearchPage";
 
 function App() {
   return (
     <div className="app">
-      <Header  />
-      <Home  />
-      <Footer  />
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
