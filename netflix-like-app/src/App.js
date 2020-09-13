2f2ba2c216da2a858123d1ee6038380c810d5636
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-09-13 01:22:49 -0500

    Row component finished

--- netflix-like-app/src/App.js
+++ netflix-like-app/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './App.css';
 import Row from './components/Row/Row';
+import requests from './client/tmdb/requests';
 
 // api key firebase :  AIzaSyB0Bx51LVf6JqEFEUhh3MrY4I0VVjS3Cok
 
 function App() {
   return (
     <div className="App">
-      <Row title="NETFLIX ORIGINALS" />
-      <Row title="Trending Now" />
+      <Row title="NETFLIX ORIGINALS" largeRow url={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" url={requests.fetchTrending}/>
+      <Row title="Top Rated" url={requests.fetchTopRated}/>
+      <Row title="Action Movies" url={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" url={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" url={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" url={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" url={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- netflix-like-app/src/components/Row/Row.js
+++ netflix-like-app/src/components/Row/Row.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import './Row.css';
+import React, { useEffect, useState } from "react";
+import "./Row.css";
+import axios from "../../client/axios";
 
-const Row = ({title}) => {
+const imageBaseUrl = "https://image.tmdb.org/t/p/original/";
 
-    return(
-        <div className="row">
-            <h2>{title}</h2>
-        </div>
-    );
+const Row = ({ title, url , largeRow}) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await axios.get(`${url}`);
+      console.log(response);
+      setMovies(response.data.results);
+    }
+    fetchData();
+  }, [url]);
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__container">
+        {movies.map( movie => (
+          <img
+            key={movie.id}
+            src={`${imageBaseUrl}${largeRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name}
+            className={`row__poster ${largeRow && "row__posterLarge"}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Row;
