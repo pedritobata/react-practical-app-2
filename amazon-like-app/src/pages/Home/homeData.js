5ebfd523fe7967e59be486fd27e8becfa65931c6
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-10-29 19:38:32 -0500

    cambios que no recuerdo

--- amazon-like-app/src/pages/Home/homeData.js
+++ amazon-like-app/src/pages/Home/homeData.js
@@ -1,10 +1,13 @@
 
-export const categoriesImages = ["https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_es_US_2x._CB432534552_.jpg",
+export const categoriesImages = [
+"https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/October/Fuji_Tallhero_Dash_es_US_2x._CB418727898_.jpg",
+"https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_es_US_2x._CB432534552_.jpg",
 "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Currency_v1_es_US_1x._CB428993289_.jpg",
 "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Toys_es_US_2x._CB431858162_.jpg",
 "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Beauty_v2_es_US_2x._CB429092343_.jpg",
 "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Home_es_US_2x._CB428980068_.jpg",
-"https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Sports_es_US_2x._CB431860454_.jpg"];
+"https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Sports_es_US_2x._CB431860454_.jpg"
+];
         
 
 export const servicesItemsData = [
