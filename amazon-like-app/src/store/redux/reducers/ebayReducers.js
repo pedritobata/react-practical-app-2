675f095f4ea8e1bda5f0b4cb92ea72929dc22d32
pedro martinez <pedromartinez_goyo@hotmail.com>
2020-11-10 05:52:39 -0500

    configurations changes , switched to production Ebay environment - frontend and backend

--- amazon-like-app/src/store/redux/reducers/ebayReducers.js
+++ amazon-like-app/src/store/redux/reducers/ebayReducers.js
@@ -3,19 +3,21 @@ import { AUTH_EBAY_CONSENT_REQUEST } from '../constants/ebayConstants';
 
 // CREAMOS LA URL PARA REDIRECCIONAR A LA PAGINA DE CONSENTIMIENTO DEL USER EN EBAY
 const clientId = "PedroMar-myamazon-SBX-87cf12021-46a66edc";
+const clientId_prod = "PedroMar-myamazon-PRD-27a727799-eba6a00b";
 const redirectId = "Pedro_Martinez-PedroMar-myamaz-hlemvy";//RuName
-const scope = encodeURIComponent("https://api.ebay.com/oauth/api_scope/buy.order.readonly");
+const redirectId_prod = "Pedro_Martinez-PedroMar-myamaz-wpssjlmv";
+const scope = encodeURIComponent("https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/commerce.identity.readonly");
 const state = "";
 const prompt = "login";
-const consentUrl = `https://auth.sandbox.ebay.com/oauth2/consents?client_id=${clientId}&redirect_uri=${redirectId}&response_type=code&scope=${scope}&prompt=${prompt}`;
+const consentUrl = `https://auth.ebay.com/oauth2/consents?client_id=${clientId_prod}&redirect_uri=${redirectId_prod}&response_type=code&scope=${scope}&prompt=${prompt}`;
 
 
-export const authEbayReducer = (state = {consentUrl, redirectId}, action) => {
+export const authEbayReducer = (state = {consentUrl, redirectId_prod}, action) => {
     switch(action.type){
         case AUTH_EBAY_CONSENT_REQUEST:
             return {
                 loading: true,
-                redirectId,
+                redirectId_prod,
                 consentUrl
             }
         default:
--- amazon-like-app/src/store/redux/sagas/ebaySagas.js
+++ amazon-like-app/src/store/redux/sagas/ebaySagas.js
@@ -7,31 +7,18 @@ import {
 import { put,select } from "redux-saga/effects";
 import axios from "axios";
 
-// DATA DE EBAY PARA SOLICITAR TOKEN DE ACCESSO A NIVEL APP
-const ebayClientId = "PedroMar-myamazon-SBX-87cf12021-46a66edc";
-const ebayClientSecret = "SBX-7cf120211133-04b3-4965-ac62-b1cf";  
-const encodedCredentials = btoa(
-  ebayClientId.concat(":").concat(ebayClientSecret)
-);
-
-const scopeList =
-  "https://api.ebay.com/oauth/api_scope https://api.ebay.com/oauth/api_scope/buy.order.readonly https://api.ebay.com/oauth/api_scope/buy.guest.order https://api.ebay.com/oauth/api_scope/sell.marketing.readonly https://api.ebay.com/oauth/api_scope/sell.marketing https://api.ebay.com/oauth/api_scope/sell.inventory.readonly https://api.ebay.com/oauth/api_scope/sell.inventory https://api.ebay.com/oauth/api_scope/sell.account.readonly https://api.ebay.com/oauth/api_scope/sell.account https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly https://api.ebay.com/oauth/api_scope/sell.fulfillment https://api.ebay.com/oauth/api_scope/sell.analytics.readonly https://api.ebay.com/oauth/api_scope/sell.marketplace.insights.readonly https://api.ebay.com/oauth/api_scope/commerce.catalog.readonly https://api.ebay.com/oauth/api_scope/buy.shopping.cart https://api.ebay.com/oauth/api_scope/buy.offer.auction https://api.ebay.com/oauth/api_scope/commerce.identity.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly https://api.ebay.com/oauth/api_scope/commerce.identity.status.readonly https://api.ebay.com/oauth/api_scope/sell.finances https://api.ebay.com/oauth/api_scope/sell.item.draft https://api.ebay.com/oauth/api_scope/sell.payment.dispute https://api.ebay.com/oauth/api_scope/sell.item";
-
-const testSingleScope = "https://api.ebay.com/oauth/api_scope/buy.order.readonly";
-
-
 
 // ASK FOR USER ACCESS TOKEN
 export function* authEbaySaga(action) {
   yield put({ type: AUTH_EBAY_REQUEST });
-  const {redirectId} = yield select(state => state.authEbay);
+  const {redirectId_prod} = yield select(state => state.authEbay);
   try {
     const response = yield axios.get(
       `http://localhost:5001/like-app-94bda/us-central1/api/token`,
 
       {
         params: {
-          redirectUri: redirectId,
+          redirectUri: redirectId_prod,
           code: action.authCode
         }
     }
